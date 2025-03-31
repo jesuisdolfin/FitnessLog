ae68ebc26b2ce77686d85ced62c292ae4ebd43ce
jesuisdolfin <charlie.dolphin9@gmail.com>
2025-03-30 20:58:12 -0500

    fixed BASE_URL in config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-//const BASE_URL = "http://18.191.252.100";  // EC2 IP
-const BASE_URL = "http://localhost";  // For local development and testing
+const BASE_URL = "http://18.191.252.100";  // EC2 IP
+// const BASE_URL = "http://localhost";  // For local development and testing
 export default BASE_URL;

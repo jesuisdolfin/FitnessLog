8fd9617214dfc75c845eabd46af0fc583593a914
jesuisdolfin <charlie.dolphin9@gmail.com>
2025-03-30 20:21:54 -0500

    added different BASE_URL to config.js for local testing

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,3 @@
-const BASE_URL = "http://18.191.252.100";  // Replace with your actual EC2 IP
+//const BASE_URL = "http://18.191.252.100";  // EC2 IP
+const BASE_URL = "http://localhost";  // For local development and testing
 export default BASE_URL;
